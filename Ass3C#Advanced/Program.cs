6ef31405143ee79bb6063544c9b7e8c497159a01
Admin <Admin@SALMAAMRO>
2025-08-26 22:12:13 +0300

    Q1

--- Ass3C#Advanced/Program.cs
+++ Ass3C#Advanced/Program.cs
@@ -1,10 +1,44 @@
 ﻿namespace Ass3C_Advanced
 {
     internal class Program
-    {
+    {     
+           #region Q1
+      //public static int NonRepeatedCharcter (string str)
+      //{
+      // Dictionary<char, int> Count = new Dictionary<char, int>();
+
+      //   for(int i=0;i<str.Length;i++)
+      //   {
+      //          if (Count.ContainsKey(str[i]))
+      //              Count[str[i]]++;
+                
+      //          else
+      //              Count[str[i]] = 1;
+      //   }
+      //   for(int i=0;i<str.Length;i++)
+      //   {
+      //          if (Count[str[i]] == 1)
+      //          {
+      //              return i;
+      //          }
+
+                   
+      //   }
+
+
+      //          return -1;
+      //}
+
+        #endregion
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region CallQ1
+            //string str = "Salmokka";
+
+            //Console.WriteLine($"Index : {NonRepeatedCharcter(str)}");
+            #endregion
+
         }
     }
 }
